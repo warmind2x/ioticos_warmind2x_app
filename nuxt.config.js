13733a050de741e00299314cfea46e536db1eca4
warmind2x <jgarban@outlook.com>
2021-07-25 19:13:46 -0400

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,6 +65,18 @@ export default {
     mqtt_port:process.env.MQTT_PORT
   },
 
+  server:{
+    port: 3000,
+    host: '0.0.0.0',
+
+  },
+
+  serverMiddleware:{
+     '/api': '~/api'
+
+
+  },
+
   /*
   ** Build configuration
   */
